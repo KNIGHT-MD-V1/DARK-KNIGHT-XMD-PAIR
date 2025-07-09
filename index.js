7b9c5931b664c9733e51ef2545443effedda3b8c
KNIGHT-MD-V1 <botkavidu3@gmail.com>
2025-07-09 16:25:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
 let code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/server', server);
 app.use('/code', code);
 app.use('/pair',async (req, res, next) => {
 res.sendFile(__path + '/pair.html')
